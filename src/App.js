9ac6c7a756b5bcc7e2401fa15f14108b66f82704
Alvin Ng <ngalvin93@gmail.com>
2019-11-25 19:44:04 -0600

    complete get and render api

--- src/App.js
+++ src/App.js
@@ -9,3 +9,12 @@ function App() {
 }
 
 export default App;
+
+// questions
+// package.json = how does the proxy connection to the api work under the hood?
+// how to do relative path in linking style sheets?
+// when and when not to use React class?
+// is a constructor() only used in classes or can it work in functional components?
+// not sure what constructor() or super() is doing in classes
+// i had to add cors to the api, why?
+
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,7 +3,25 @@ import '/Users/alvinng/DigitalCrafts/class-exercise/112319_react_job_api/react-j
 import JobsListItem from './JobsListItem'
 import axios from 'axios'
 
-const Jobs = () => {
+class Jobs extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            jobs: []
+        }
+    }
+
+    componentDidMount () {
+        axios.get('http://localhost:3001/api/jobs')
+            .then(({data})=>{this.setState({ jobs: data })})
+            .catch((error)=>{console.log('axios error!!!', error)})
+    }
+
+    render () {
+    let jobsJSX = this.state.jobs.map((job, index)=>{
+        return <JobsListItem key={index} {...job}/>
+    })
+
     return (
         <div>
             <div className="App">
@@ -12,9 +30,9 @@ const Jobs = () => {
                     <p className="App-subtitle">Click to explore jobs</p>
                 </header>
             </div>
-            <div className="Jobs"><JobsListItem /><JobsListItem /><JobsListItem /></div>
+            <div className="Jobs">{ jobsJSX }</div>
         </div>
-    )
+    )}
 }
 
 export default Jobs
